150782009c0c2855cc9dee8d4f9dd96d03f6892d
cita <giacomomastrone@gmail.com>
2026-01-20 13:43:13 +0100

    Refactor Cartella class with comments

--- Cartella.java
+++ Cartella.java
@@ -5,17 +5,20 @@ public class Cartella {
     private Set<Integer> numeri;
     private static final int NUMERI_CARTELLA = 15;
 
+    //costruttore che crea un hashset vuoto
     public Cartella() {
         numeri = new HashSet<>();
         while (numeri.size() < NUMERI_CARTELLA) {
-            numeri.add((int) (Math.random() * 90) + 1);  // Genera numeri casuali da 1 a 90
+            numeri.add((int) (Math.random() * 90) + 1);  
         }
     }
 
+    //metodo getter che restituisce l'insieme dei numeri della cartella
     public Set<Integer> getNumeri() {
         return numeri;
     }
 
+    //controlla se un numero è presente nella cartella(true se cè,false se non cè
     public boolean contieneNumero(int numero) {
         return numeri.contains(numero);
     }
@@ -24,3 +27,4 @@ public class Cartella {
         return 1.0;  // Costo fisso per ogni cartella
     }
 }
+
