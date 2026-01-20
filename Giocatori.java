ed34afbd9163398db8af4da2bfc9c6a475344e7f
cita <giacomomastrone@gmail.com>
2026-01-20 13:47:12 +0100

    Update Giocatori.java

--- Giocatori.java
+++ Giocatori.java
@@ -6,7 +6,8 @@ public class Giocatori {
     private List<Cartella> cartelle = new ArrayList<>();
     private int numeroGiocatori;
 
-    public void aggiungiGiocatori() {
+    public void aggiungiGiocatori() {  //è il metodo principale della classe: crea i giocatori e le loro cartelle.
+        // leggi i numeri dei giocatori
         Scanner scanner = new Scanner(System.in);
         System.out.print("Quanti giocatori vogliono partecipare? (min 2, max 8): ");
         numeroGiocatori = scanner.nextInt();
@@ -17,6 +18,7 @@ public class Giocatori {
             return;
         }
 
+        // creo le cartelle
         for (int i = 0; i < numeroGiocatori; i++) {
             Cartella cartella = new Cartella();
             cartelle.add(cartella);
@@ -24,11 +26,14 @@ public class Giocatori {
         }
     }
 
+    // restituisce tutte le certelle
     public List<Cartella> getCartelle() {
         return cartelle;
     }
 
+    // restituisce i giocatori inseriti
     public int getNumeroGiocatori() {
         return numeroGiocatori;
     }
 }
+
